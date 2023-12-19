8dc94dd02976c0da16da229b5b5627c05a9793b4
KUEUNJI <112163447+EunJiKU8151@users.noreply.github.com>
2023-12-19 17:53:39 +0900

    연락처

--- src/components/commonUi/ContactUi.js
+++ src/components/commonUi/ContactUi.js
@@ -4,51 +4,75 @@ import { allUserApi, contactApi } from '../../api/index';
 
 const ContactUi = () => {
   const userIdValue = useSelector(state => state.user.userId);
+  const userImgUrl = useSelector(state => state.user.userImgUrl);
   const test = useSelector(state => state.user.accessToken);
+  const [depthItem, setDepthItem] = useState([]);
+  const [teamInfoItem, setTeamInfoItem] = useState([]);
   
-  useEffect(() => {
-    console.log(userIdValue);
-    console.log(test);
-    allUserApi()
-      .then((response) => {
-        response.data.forEach(item => {
-          if(item.EMAIL === userIdValue) {
-            console.log(item.EMAIL);
-            console.log(test);
-          }
-        });
-      })
-      .catch(err => console.log("연락처 에러!!!"))
-  }, []);
-
-  // const userImgUrl = useSelector(state => state.user.userImgUrl);
-  // const userIdValue = useSelector(state => state.user.userId);
-  // // const [depthNum, setDepthNum] = useState("");
-  // const [depthItem, setDepthItem] = useState([]);
-
   // useEffect(() => {
-  //   const fetchData = async () => {
-  //     try {
-  //       const allUserItem = await allUserApi();
-  //       const myUserItem = allUserItem.data.find(item => item.EMAIL === userIdValue);
+    
 
-  //       if (myUserItem) {
-  //         // setDepthNum(myUserItem.DEPT_CODE);
-  //         const contactUserItem = await contactApi(myUserItem.DEPT_CODE);
-  //         setDepthItem(contactUserItem.data);
-  //       }
-  //     } catch (error) {
-  //       console.error("에러 발생: ", error);
-  //     }
-  //   };
+  //   allUserApi()
+  //     .then((response) => {
+  //       response.data.forEach(item => {
+  //         if(item.EMAIL === userIdValue) {
+  //           console.log(item.EMAIL);
+  //         }
+  //       });
+  //     })
+  //     .catch(err => console.log("연락처 에러!!!"))
+  // }, [userIdValue, test]);
 
-  //   fetchData();
-  // }, []);
+
+  useEffect(() => {
+
+    const fetchData = async () => {
+      try {
+        const { data } = await allUserApi();
+        data.forEach(item => {
+          if(item.EMAIL === userIdValue) {
+            console.log(`연락처 DEPT_CODE : ${item.DEPT_CODE}`);
+            setDepthItem(item.DEPT_CODE);
+          }
+        })
+      }
+      catch(error) {
+        console.log("내 정보 에러 : " + error);
+        console.log(error);
+      }
+      console.log(depthItem);
+
+      contactApi(depthItem)
+        .then(({ data }) => {
+          console.log(data);
+          setTeamInfoItem(data);
+        })
+        .catch(error => {
+          console.log("연락처 에러 : " + error);
+          console.log(error);
+        })
+
+      // try {
+      //   const allUserItem = await allUserApi();
+      //   const myUserItem = allUserItem.data.find(item => item.EMAIL === userIdValue);
+
+      //   if (myUserItem) {
+      //     // setDepthNum(myUserItem.DEPT_CODE);
+      //     const contactUserItem = await contactApi(myUserItem.DEPT_CODE);
+      //     setDepthItem(contactUserItem.data);
+      //   }
+      // } catch (error) {
+      //   console.error("에러 발생: ", error);
+      // }
+    };
+
+    fetchData();
+  }, [depthItem]);
 
   return (
     <section className='contact-sec sec'>
-      {/* {
-        depthItem.map(item => {
+      {
+        teamInfoItem.map(item => {
           return (
             <div className="contact-list" key={item.id}>
               <a className="item">
@@ -66,7 +90,7 @@ const ContactUi = () => {
             </div>
           )
         })
-      } */}
+      }
     </section>
   )
 }
